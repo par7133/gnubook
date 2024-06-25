1dc99173f7f6ec33fc58fb15d19ebfda7455a39e
Capitan Cloud <my25mb@has.im>
2024-06-25 15:05:14 +0200

    Add files via upload

---
+++ Public/index.php
@@ -0,0 +1,229 @@
+<?php
+
+/**
+ * Copyright (c) 2016, 2024, 5 Mode
+ * 
+ * This file is part of Faceborg.
+ * 
+ * Faceborg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Faceborg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.  
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Faceborg. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * index.php
+ * 
+ * Faceborg index file.
+ *
+ * @author Daniele Bonini <my25mb@aol.com>
+ * @copyrights (c) 2016, 2024, 5 Mode     
+ */
+
+require "../Private/core/init.inc";
+
+
+// FUNCTION AND VARIABLE DECLARATIONS
+$scriptPath = APP_SCRIPT_PATH;
+
+// PARAMETERS VALIDATION
+
+$url = filter_input(INPUT_GET, "url")??"";
+$url = strip_tags($url);
+$url = strtolower(trim(substr($url, 0, 300), "/"));
+
+switch ($url) {
+  case "action":
+    $scriptPath = APP_AJAX_PATH;
+    define("SCRIPT_NAME", "action");
+    define("SCRIPT_FILENAME", "action.php");     
+    break;
+  case "":
+  case "home":
+  
+    if (defined("APP_HOME_PATH") && (APP_HOME_PATH !== PHP_STR) && is_readable(APP_HOME_PATH)) {
+      define("SCRIPT_NAME", "homeproxy");
+      define("SCRIPT_FILENAME", "homeproxy.php");   
+    } else {
+      
+      define("SCRIPT_NAME", "home");
+      define("SCRIPT_FILENAME", "home.php");   
+    
+      $pattern = APP_DATA_PATH . DIRECTORY_SEPARATOR . "*";
+      $aAvatarPaths = glob($pattern, GLOB_ONLYDIR);
+      if (empty($aAvatarPaths)) {
+        die("<br>&nbsp;No avatar exists yet: type in the url with your avatar name like http://" . $_SERVER['HTTP_HOST']. "/&lt;your avatar&gt;.<br>&nbsp;Login with the password and drag-n-drop here all the resources you want to associate to it. <br><br>&nbsp;Links by text and first dropped picture will be your avatar image.");
+      } else {
+        define("AVATAR_NAME", basename($aAvatarPaths[0]));
+      }
+    }
+    
+    break;  
+
+  case "doc":
+    $avatar = filter_input(INPUT_GET, "av")??"";
+    $avatar = strip_tags($avatar);
+        
+    $AVATAR_PATH = APP_DATA_PATH . DIRECTORY_SEPARATOR . $avatar;
+    
+    $repo = filter_input(INPUT_GET, "re")??"";
+    $repo = strip_tags($repo);
+    switch ($repo) {
+      case "cv":
+        $REPO_PATH = $AVATAR_PATH . DIRECTORY_SEPARATOR . "cv";     
+        break;
+      default:
+        die("unknown parma value:".$repo);
+    }
+    
+    $doc = filter_input(INPUT_GET, "doc")??"";
+    $doc = strip_tags($doc);
+       
+    $originalFilename = pathinfo($doc, PATHINFO_FILENAME);
+    $destFilename = explode("|",$originalFilename)[1];
+    $originalFileExt = pathinfo($doc, PATHINFO_EXTENSION);
+    $fileExt = strtolower(pathinfo($doc, PATHINFO_EXTENSION));
+    
+    $docPath = $REPO_PATH . DIRECTORY_SEPARATOR . $doc;
+       
+    if (filesize($docPath) <= APP_FILE_MAX_SIZE) { 
+      switch ($fileExt) {
+        case "doc":
+          header("Content-Type: application/msword");
+          header('Content-Disposition: attachment; filename=' . $destFilename . '.doc');
+          break;
+        case "pdf":
+          header("Content-Type: application/pdf");
+          header('Content-Disposition: attachment; filename=' . $destFilename . '.pdf');
+          break;
+        default:
+          die("unknown file extension.");
+      }
+      echo(file_get_contents($docPath));
+      exit(0);
+    } else {
+      die("doc size over app limits.");
+    }  
+    
+    break;
+  case "img":
+    $avatar = filter_input(INPUT_GET, "av")??"";
+    $avatar = strip_tags($avatar);    
+    
+    $AVATAR_PATH = APP_DATA_PATH . DIRECTORY_SEPARATOR . $avatar;
+    $GALLERY_PATH = $AVATAR_PATH . DIRECTORY_SEPARATOR . "gallery";     
+
+    $pic = filter_input(INPUT_GET, "pic")??"";
+    $pic = strip_tags($pic);
+       
+    $originalFilename = pathinfo($pic, PATHINFO_FILENAME);
+    $originalFileExt = pathinfo($pic, PATHINFO_EXTENSION);
+    $fileExt = strtolower(pathinfo($pic, PATHINFO_EXTENSION));
+    
+    if ($pic === APP_DEF_PROFILE_PIC) {
+      $picPath = APP_PATH . DIRECTORY_SEPARATOR . "static" . $pic;
+    } else {
+      $picPath = $GALLERY_PATH . DIRECTORY_SEPARATOR . $pic;
+    }  
+       
+    if (filesize($picPath) <= APP_FILE_MAX_SIZE) { 
+      if ($fileExt = "jpg") {
+        header("Content-Type: image/jpeg");
+      } else {
+        header("Content-Type: image/" . $fileExt);
+      }  
+      echo(file_get_contents($picPath));
+      exit(0);
+    } else {
+      die("picture size over app limits.");
+    }  
+    
+    break;
+  case "imgj":
+    $avatar = filter_input(INPUT_GET, "av")??"";
+    $avatar = strip_tags($avatar);
+    $jar = (int)substr(strip_tags(filter_input(INPUT_GET, "jar")??""),0,1);
+    if ($jar >= 1 && $jar <= 3) {
+    } else {
+      die("jar parameter error.");
+    }
+    
+    $AVATAR_PATH = APP_DATA_PATH . DIRECTORY_SEPARATOR . $avatar;
+    $JAR_PATH = $AVATAR_PATH . DIRECTORY_SEPARATOR . "magicjar" . $jar;     
+
+    $fileName = filter_input(INPUT_GET, "fn")??"";
+    $fileName = strip_tags($fileName);
+       
+    $originalFilename = pathinfo($fileName, PATHINFO_FILENAME);
+    $orioriFilename = explode("|", $originalFilename)[1];
+    $originalFileExt = pathinfo($fileName, PATHINFO_EXTENSION);
+    $fileExt = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
+    
+    $filePath = $JAR_PATH . DIRECTORY_SEPARATOR . $fileName;
+       
+    if (filesize($filePath) <= APP_FILE_MAX_SIZE) { 
+      if ($fileExt = "jpg") {
+        header("Content-Type: image/jpeg");
+      } else {
+        header("Content-Type: image/" . $fileExt);
+      }  
+      //header("Content-Disposition: attachment; filename=" . $orioriFilename . ".$fileExt");
+      echo(file_get_contents($filePath));
+      exit(0);
+    } else {
+      die("file size over app limits.");
+    }  
+    
+    break;    
+  case "file":
+    $avatar = filter_input(INPUT_GET, "av")??"";
+    $avatar = strip_tags($avatar);
+    $jar = (int)substr(strip_tags(filter_input(INPUT_GET, "jar")??""),0,1);
+    if ($jar >= 1 && $jar <= 3) {
+    } else {
+      die("jar parameter error.");
+    }
+    
+    $AVATAR_PATH = APP_DATA_PATH . DIRECTORY_SEPARATOR . $avatar;
+    $JAR_PATH = $AVATAR_PATH . DIRECTORY_SEPARATOR . "magicjar" . $jar;     
+
+    $fileName = filter_input(INPUT_GET, "fn")??"";
+    $fileName = strip_tags($fileName);    
+    
+    $originalFilename = pathinfo($fileName, PATHINFO_FILENAME);
+    $orioriFilename = explode("|", $originalFilename)[1];
+    $originalFileExt = pathinfo($fileName, PATHINFO_EXTENSION);
+    $fileExt = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
+    
+    $filePath = $JAR_PATH . DIRECTORY_SEPARATOR . $fileName;
+       
+    if (filesize($filePath) <= APP_FILE_MAX_SIZE) { 
+      header("Content-Type: avatarfree/bin");
+      header("Content-Disposition: attachment; filename=" . $orioriFilename . ".$fileExt");
+      echo(file_get_contents($filePath));
+      exit(0);
+    } else {
+      die("file size over app limits.");
+    }  
+    
+    break;    
+  default:
+    define("SCRIPT_NAME", "home");
+    define("SCRIPT_FILENAME", "home.php");   
+
+    define("AVATAR_NAME", $url);
+
+    break;
+}
+
+if (SCRIPT_NAME==="err-404") {
+  header("HTTP/1.1 404 Not Found");
+}  
+
+require $scriptPath . "/" . SCRIPT_FILENAME;
---
+++ Public/static/js/home-js.php
@@ -0,0 +1,368 @@
+<?PHP
+/**
+ * Copyright (c) 2016, 2024, 5 Mode
+ * 
+ * This file is part of Faceborg.
+ * 
+ * Faceborg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Faceborg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.  
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Faceborg. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * home-js.php
+ * 
+ * Faceborg js for the Home page.
+ *
+ * @author Daniele Bonini <my25mb@aol.com>
+ * @copyrights (c) 2016, 2024, 5 Mode     
+ */
+ 
+ require "../../../Private/core/init.inc";
+
+ header("Content-Type: text/javascript");
+
+ // PARAMETERS AND VARIABLES INIT
+ 
+ $lang = APP_DEF_LANG;
+ $lang1 = substr(filter_input(INPUT_GET, "hl")??"", 0, 5);
+ $lang1= strip_tags($lang1);
+ if ($lang1 !== PHP_STR) {
+   $lang = $lang1;
+ }
+ $shortLang = getShortLang($lang);
+ 
+ $AVATAR_NAME=filter_input(INPUT_GET, "av")??"";
+ $AVATAR_NAME= strip_tags($AVATAR_NAME);
+ 
+ $CURRENT_VIEW=filter_input(INPUT_GET, "cv")??"";
+ $CURRENT_VIEW= strip_tags($CURRENT_VIEW);
+ 
+ $CUDOZ=filter_input(INPUT_GET, "cu")??"";
+ $CUDOZ= strip_tags($CUDOZ);
+?>
+ 
+ var myToolsOnIntID;
+
+ function showHowTo() {
+ 
+ <?PHP if ($lang == PHP_EN): ?>
+ 
+   alert("Here how to manage your avatars in Faceborg\n" +
+         "- Type in the url of your avatar like http://" + "<?PHP echo($_SERVER['HTTP_HOST']);?>" + "/<your avatar>.\n" +
+         "- Login with your pasword.\n" +
+         "- Drag-n-drop in the browser window all the resources you like: \n" +
+         "   a. Drop in txt files to shape your blog;\n" +
+         "   a. Drop in pic to shape your gallery; the first one as yr avatar picture.\n" +
+         "   b. Drop in texts with link separeted by <ENTER> to shape your friends.\n" +
+         "\n" +
+         "Enjoy!"); 
+         
+  <?PHP elseif ($lang == PHP_IT): ?>       
+
+   alert("Ecco come gestire i tuoi avatar in Faceborg:\n" +
+         "- Scrivi l'url del tuo avatar come http://" + "<?PHP echo($_SERVER['HTTP_HOST']);?>" + "/<tuo avatar>.\n" +
+         "- Loggati con la pasword.\n" +
+         "- Fai il drag-n-drop nella finestra del browser di tutte le risorse che ritieni: \n" +
+         "   a. Trascina .txt file per creare il blog\n" +
+         "   b. Trascina le tue immagini per creare la galleria, la prima sara' il tuo avatar.\n" +
+         "   b. Trascina link da testo separati da <INVIO> per creare i tuoi amici.\n" +
+         "\n" +
+         "Buon proseguimento!"); 
+         
+  <?PHP elseif ($lang == PHP_CN): ?>              
+
+   alert("在这里如何免费管理您的化身:\n" +
+         "- 以以下格式输入阿凡达的URLhttp://" + "<?PHP echo($_SERVER['HTTP_HOST']);?>" + "/<你的头像>.\n" +
+         "- 使用密码登录.\n" +
+         "- 在浏览器窗口中拖动n-drop您喜欢的所有资源: \n" +
+         "   a. 放入.txt文件以形成您的博客.\n" +
+         "   b. 放入图片以塑造画廊； 第一个作为您的头像.\n" +
+         "   b. 删除带有链接<enter>分隔的链接以塑造您的朋友的文本.\n" +
+         "\n" +
+         "Enjoy!"); 
+         
+  <?PHP endif; ?>                
+ }  
+
+function settingsOn() {
+  $(".settingson").hide();
+  $(".magicjar1").show();
+  $(".magicjar2").show();
+  $(".magicjar3").show();
+  $(".settingsoff").show();
+  setTimeout("settingsOff()",6000);
+}  
+
+function settingsOff() {
+  $(".settingsoff").hide("slow");
+  $(".magicjar1").hide("slow");
+  $(".magicjar2").hide("slow");
+  $(".magicjar3").hide("slow");
+  $(".settingson").show();
+}  
+
+function toolsOn() {
+  settingsOn();
+  $(".tools").show("slow");
+    
+  clearInterval(myToolsOnIntID);
+}  
+
+function setJar1On() {
+  $(".magicjar1").css("background","url(/res/magicjar1.png)");
+  $(".magicjar1").css("background-size","120px 120px");
+  document.getElementsByClassName("magicjar1")[0].onclick=setJar1Off;
+  document.getElementById("txtMagicJar1").value="1";
+  document.getElementById("frmUpload").submit();
+}
+
+function setJar1Off() {
+  $(".magicjar1").css("background","url(/res/magicjar1dis.png)");
+  $(".magicjar1").css("background-size","120px 120px");
+  document.getElementsByClassName("magicjar1")[0].onclick=setJar1On;
+  document.getElementById("txtMagicJar1").value="0"; 
+  document.getElementById("frmUpload").submit();
+}
+
+function setJar2On() {
+  $(".magicjar2").css("background","url(/res/magicjar2.png)");
+  $(".magicjar2").css("background-size","120px 120px");
+  document.getElementsByClassName("magicjar2")[0].onclick=setJar2Off;
+  document.getElementById("txtMagicJar2").value="1"; 
+  document.getElementById("frmUpload").submit();
+}
+
+function setJar2Off() {
+  $(".magicjar2").css("background","url(/res/magicjar2dis.png)");
+  $(".magicjar2").css("background-size","120px 120px");
+  document.getElementsByClassName("magicjar2")[0].onclick=setJar2On;
+  document.getElementById("txtMagicJar2").value="0";
+  document.getElementById("frmUpload").submit();
+}
+
+function setJar3On() {
+  $(".magicjar3").css("background","url(/res/magicjar3.png)");
+  $(".magicjar3").css("background-size","120px 120px");
+  document.getElementsByClassName("magicjar3")[0].onclick=setJar3Off;
+  document.getElementById("txtMagicJar3").value="1";
+  document.getElementById("frmUpload").submit();
+}
+
+function setJar3Off() {
+  $(".magicjar3").css("background","url(/res/magicjar3dis.png)");
+  $(".magicjar3").css("background-size","120px 120px");
+  document.getElementsByClassName("magicjar3")[0].onclick=setJar3On;
+  document.getElementById("txtMagicJar3").value="0";
+}
+ 
+ function startApp() {
+
+   hidePassword();
+   
+ }			
+
+ function hidePassword() {
+   $("#passworddisplay").css("visibility","hidden");
+ }  
+
+ /*
+  * call to startApp
+  * 
+  * @returns void
+  */
+ function _startApp() {
+   
+   setTimeout("startApp()", 1000);    
+ }
+ 
+/*
+ *  Display the current hash for the config file
+ *  
+ *  @returns void
+ */
+function showEncodedPassword() {
+  if ($("#Password").val() === "") {
+    $("#Password").addClass("emptyfield");
+    return;  
+  }
+  //if ($("#Salt").val() === "") {
+  //  $("#Salt").addClass("emptyfield");
+  //  return;  
+  //}	   	
+  passw = encryptSha2( $("#Password").val() + $("#Salt").val());
+  msg = "<?PHP echo(getResource0("Please set your hash in the config file with this value", $lang, "/js/home-js.php"));?>:";
+  alert(msg + "\n\n" + passw);	
+}
+
+function changeLang(tthis) {
+  window.open("/<?PHP echo($AVATAR_NAME);?>?hl="+$(tthis).val(),"_self");
+}
+
+function reload() {
+  //window.location.reload(); 
+  document.getElementById("frmUpload").submit();
+}
+
+$("div.dragover").on("dragover", function(e) {
+  e.stopPropagation();
+  e.preventDefault();
+
+  e.originalEvent.dataTransfer.dropEffect = "copy";
+
+  return false;
+});
+
+$("div.dragover").on("drop", function(e) {
+  e.stopPropagation();
+  e.preventDefault();
+
+  // Get the current Upload form obejct..
+  var form = document.getElementById("frmUpload");
+  // Create a FormData object including the actual form data..
+  var fd = new FormData(form);
+
+  // Get the array of files dropped..
+  var dt = e.originalEvent.dataTransfer;
+  var files = dt.files;
+
+  var count = files.length;
+  //alert("File Count: " + count + "\n");
+
+  if (count !== 0) {
+
+    for (var i = 0; i < count; i++) {
+      //alert(" File " + i + ":\n(" + (typeof files[i]) + ") : <" + files[i] + " > " +
+      //       files[i].name + " " + files[i].size + " " + files[i].type + "\n");
+      if (files[i].size > <?PHP echo(APP_FILE_MAX_SIZE); ?>) {
+        alert("ERROR: file size (" + files[i].size +") exceeds app limit: <?PHP echo(APP_FILE_MAX_SIZE); ?>");
+        return;
+      }
+      
+      fd.append("filesdd[]", files[i]);
+    }
+
+    // Submit of the FormData..
+    $.ajax("/<?PHP echo($AVATAR_NAME);?>", {
+        method: "POST",
+        processData: false,
+        contentType: false,
+        data: fd//,
+        //success: function (data) {
+        //  $("body").html(data);
+        //}
+    });
+      
+    setTimeout("reload()", 2000); 
+    
+  } else {
+    
+    mytext = e.originalEvent.dataTransfer.getData('text/plain');
+    re = new RegExp(/(https?:\/\/)([\da-z\.-]+)\.([a-z\.]{2,8})(\/?.+)?$/gum);
+    matches = mytext.matchAll(re);
+    ffriends="";
+    if (matches !== null) {
+      for(const match of matches) {
+        if (ffriends==="") {
+          ffriends+=match[0];
+        } else {
+          ffriends+="|"+match[0];          
+        }  
+      }
+    }
+        
+    if (ffriends!=="") {
+      
+      //fd.append("f", ffriends);
+      document.getElementById("f").value = ffriends;  
+      
+      //alert(document.getElementById("f").value);
+      
+      document.getElementById("frmUpload").submit();
+      
+    } else {
+      //alert("ale!");
+    }  
+  }
+
+  return false;
+});
+
+$("input#files").on("change", function(e) {
+  frmUpload.submit();
+});
+
+function setContentPos2() {                    
+  h=parseInt(window.innerHeight);
+  w=parseInt(window.innerWidth);
+
+  <?PHP if ($CURRENT_VIEW ==ADMIN_VIEW): ?>
+  $("#fireupload").css("top", ((h - 255) / 2) + "px");
+  $("#fireupload").css("left", ((w - 255) / 2) + "px");
+  $("#fireupload").css("display", "inline");
+  //$("#picavatar").css("top", ((h - 255) / 2) + "px");
+  //$("#picavatar").css("left", ((w - 255) / 2) + "px");
+  $("#picavatar").css("display", "inline");
+ <?PHP else: ?> 
+  if (window.innerWidth<800) {
+    $("#header").css("display", "none");
+    $("#headerMob").css("display", "inline");    
+    $("#cudoz").css("display", "none");
+  } else {
+    $("#header").css("display", "inline");
+    $("#headerMob").css("display", "none");    
+    $("#cudoz").css("display", "inline");
+  }   
+  <?PHP endif; ?>
+  $(".dragover").css("height", h + "px");
+  $(".dragover").css("width", w + "px");
+
+  newleft=parseInt(window.innerWidth - 145);
+  $(".tools").css("left",newleft+"px");
+  
+  mytop = parseInt(window.innerHeight - ($("#passworddisplay").height() + 60));
+  $("#passworddisplay").css("top", mytop+"px");
+} 
+
+function setFooterPos() {
+  if (document.getElementById("footerCont")) {
+    tollerance = 16;
+    $("#footerCont").css("top", parseInt( window.innerHeight - $("#footerCont").height() - tollerance ) + "px");
+    $("#footer1").css("top", parseInt( window.innerHeight - $("#footer1").height() - tollerance ) + "px");
+    $("#footer2").css("top", parseInt( window.innerHeight - $("#footer2").height() - tollerance ) + "px");
+    $("#footer2").css("left",  parseInt( window.innerWidth - $("#footer2").width() - tollerance ) + "px");
+    $("#footer").css("top", parseInt( window.innerHeight - $("#footer").height() - tollerance ) + "px");
+  }
+}
+
+window.addEventListener("load", function() {
+
+  setTimeout("setContentPos2()", 500);
+  setTimeout("setFooterPos()", 1000);
+
+  <?PHP if ($CURRENT_VIEW ==ADMIN_VIEW): ?>
+  myToolsOnIntID = setInterval("toolsOn()", 2000);
+ <?PHP else: ?>
+  // display cudoz
+  //for (i=1;i<=<?PHP echo($CUDOZ);?>;i++) {
+  //  $("#cudozentry"+i).get(0).src="/res/chicca_<?PHP echo($shortLang);?>.png";
+  //}  
+ <?PHP endif; ?>
+  
+  setTimeout("_startApp()", 10000);
+
+}, true);
+
+window.addEventListener("resize", function() {
+
+  setTimeout("setContentPos2()", 500);
+  setTimeout("setFooterPos()", 1000);
+
+}, true);
